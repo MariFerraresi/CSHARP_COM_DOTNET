c03d983edf5166e423c0a02052ffe21014e8c09b
Mariane Ferraresi <121906185+MariFerraresi@users.noreply.github.com>
2023-09-24 20:35:40 -0300

    Aulas Iniciais CSharp
    Aprendendo a programar com C # e .Net  (Dot Net Sharp) utilizando Visual Studio vs 2019.

---
+++ PrimeiroExercicio/PrimeiroExercicio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mariane Ferraresi Pintucci");
+            Console.WriteLine("(11) 99299-6212");
+            Console.WriteLine("marianepintucci@yahoo.com.br");
+
+            Console.ReadLine();
+        }
+    }
+}
