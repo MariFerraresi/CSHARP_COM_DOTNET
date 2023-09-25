6809a3a186b3eaeeb98ed362c1475d361d4b5a82
Mariane Ferraresi <121906185+MariFerraresi@users.noreply.github.com>
2023-09-25 00:23:25 -0300

    C#

---
+++ AulaTiposPrimitivos/AulaTiposPrimitivos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaTiposPrimitivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Tipos Primitivos");
+
+            string nome = "Mariane";
+            short idade = 100;
+            int numero = 20;
+            long numerogrande = 1200000;
+            Int32 teste = 10;
+            System.Int32 outroteste = 10;
+            byte item = 20;
+
+            double comissao = 32.45787898798;
+            decimal valor = Convert.ToDecimal(1000.56);
+            decimal valor1 = 1000.56m;
+
+            DateTime data;
+            //ou
+            DateTime data1 = DateTime.Now;
+
+            DateTime dataexemplo = new DateTime(2024, 12, 31);
+
+            //formatar
+
+            Console.WriteLine("{0}", dataexemplo);
+            Console.WriteLine("{0:d}", dataexemplo);
+            Console.WriteLine("{0:D}", dataexemplo);
+            Console.WriteLine("{0:ddd}", dataexemplo);
+            Console.WriteLine("{0:dddd}", dataexemplo);
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ SegundoExercicio/SegundoExercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe o valor do produto: ");
+            decimal valor = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade adquirida:");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+
+            //processamento
+            decimal total = valor * quantidade;
+
+            Console.WriteLine("O total da sua compra é {0:c} !", total);
+            Console.ReadLine();
+
+
+        }
+    }
+}
