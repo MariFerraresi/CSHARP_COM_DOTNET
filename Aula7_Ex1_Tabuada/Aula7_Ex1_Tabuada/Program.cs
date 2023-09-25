863040ad5c2df0df4f523684eb2145890c8e163c
Mariane Ferraresi <121906185+MariFerraresi@users.noreply.github.com>
2023-09-25 18:39:45 -0300

    C#

---
+++ Aula7_Ex1_Tabuada/Aula7_Ex1_Tabuada/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula7_Ex1_Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabuada;
+            int resultado;
+
+            Console.Write("Qual a tabuada que você deseja ver? Digite um número: ");
+            tabuada = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i=1; i<=10; i++)
+                    {
+                resultado = tabuada * i;
+                Console.WriteLine("{0} X {1} = {2} ", tabuada, i, resultado);
+               
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ LogicaCompra_Ex02/LogicaCompra_Ex02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaCompra_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // formas de pagamento
+            //Se o valor da compra for maior que R$1.000 , pode parcelar em  3x.
+            // Se nã for, apenas a vista
+
+            // Declaração das Variaveis
+
+            decimal ValorCompra;
+            int NumeroParcelas = 3;
+            decimal ValorParcela;
+
+            // Obtendo os valores
+
+            Console.WriteLine("Digite o valor da compra : ");
+            ValorCompra = Convert.ToDecimal(Console.ReadLine());
+
+            // processamento
+
+            if (ValorCompra >1000)
+            {
+                ValorParcela = ValorCompra / NumeroParcelas;
+                Console.WriteLine("Você poderá dividir a sua compra em 03 parcelas de {0:c}", ValorParcela);
+                Console.ReadLine();
+
+            }
+
+            else
+            {
+                Console.WriteLine("Esta compra aceita apenas pagamento a vista");
+                Console.ReadLine();
+
+            }
+
+
+
+
+
+        }
+    }
+}
